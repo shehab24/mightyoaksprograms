fa24cd99a78a04e2b2b55840349f2301ee9a851d
shehab24 <mdshehab204@gmail.com>
2023-02-14 01:17:53 +0600

    new changes for footer

--- js/main.js
+++ js/main.js
@@ -108,3 +108,37 @@ $('.multiple_items').slick({
  
     ]
   });
+
+$('.warrior_items').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+ 
+    ]
+  });
